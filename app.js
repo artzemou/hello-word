c2638f738c26b7c43fc27372762d3e0196b00410
Benjamin Frizon de Lamotte <benjamin.frizon-de-lamotte-de-reges@mnhn.fr>
2024-12-19 16:27:59 +0100

    Création d'une route au niveau de l'api pour poster les données du formulaire d'ajout d'article

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ app.use(express.urlencoded({ extended: true }));
 // Servir la page HTML (formulaire)
 app.use(express.static(path.join(__dirname, "public")));
 
+app.post("/addArticle", (req, res) => {
+  const { title, article } = req.body;
+  const slug = title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '');
+  if (!title || !article) {
+    return res.status(400).send("Les champs 'title' et 'article' sont requis");
+  }
+
+  db.run("INSERT INTO articles (title, slug, article) VALUES (?, ?,)", [title, slug, article], function (err) {
+    if (err) {
+      res.status(500).send("Erreur lors de l'ajout de l'article");
+    } else {
+      res.redirect("/"); // Rediriger vers la page d'accueil après ajout
+    }
+  });
+});
+
+
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
